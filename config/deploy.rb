49bc42ed2a637818c8b5cf536f0f320b894d469b
thkernel <a.dembele@upcase.net>
2021-08-20 18:03:46 +0000

    add few stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :repo_url, "https://github.com/thkernel/softwinds.git"
 # set :pty, true
 
 # Default value for :linked_files is []
- append :linked_files, "config/database.yml", "config/master.key"
+ append :linked_files, "config/database.yml", "config/master.key", "config/storage.yml", "config/meta.yml"
 
 # Default value for linked_dirs is []
  append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
